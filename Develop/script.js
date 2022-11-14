9b3194b752ca941619bd7a5e00f1a4c748321b2d
Ameera Noormohamed <ameera.noormohamed@divurgent.com>
2022-11-14 00:46:04 -0800

    added function to calculate past, present, and future accordingly

--- Develop/script.js
+++ Develop/script.js
@@ -10,4 +10,20 @@ $(function () {
   for (var i = 0; i < timeslots.length; i++) {
     var button = timeslots[i].querySelector('button')
     button.addEventListener('click', saveEvent) 
+
+    var taskId = button.closest(".row").id
+    const task = JSON.parse(localStorage.getItem(taskId)) || []
+    document.getElementById(taskId).querySelector('textarea').value = task
+
+    var currentHour = now.hour()
+    var taskHour = parseInt(taskId.substring(5))
+
+    if (currentHour - taskHour == 0) {
+      document.getElementById(taskId).classList.add('present')
+    } else if (currentHour - taskHour > 0) {
+      document.getElementById(taskId).classList.add('future')
+    } else if (currentHour - taskHour < 0) {
+      document.getElementById(taskId).classList.add('past')
+    }
+  }
 });
