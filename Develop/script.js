699c3ed3642ecac5c50f167cc927b2d916c15a6c
Ameera Noormohamed <ameera.noormohamed@divurgent.com>
2022-11-14 00:44:02 -0800

    added date on header of calendar and added click function to save button

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,13 @@
 
 $(function () {
  
-  
+  var now = dayjs()
+  var currentDay = document.getElementById('currentDay')
+  currentDay.innerHTML = now.format('dddd DD/MM/YYYY h:mmA')
+
+  var timeslots = document.getElementById('timeslots').querySelectorAll('.row')
+
+  for (var i = 0; i < timeslots.length; i++) {
+    var button = timeslots[i].querySelector('button')
+    button.addEventListener('click', saveEvent) 
 });
